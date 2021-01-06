30d33a3c0003ae229edb7d3e7a901a3e8c57cc23
UMUTONIRitha <rithamarie9@gmail.com>
2021-01-06 06:19:09 +0200

    urls for project

--- instagram/urls.py
+++ instagram/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url,include
 from django.contrib import admin
+from django.contrib.auth import views 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-      url(r'^insta_app/',include('insta_app.urls'))
+    url(r'^',include('insta_app.urls')),
+    url(r'^tinymce/', include('tinymce.urls')),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
+    url(r'^logout/$', views.logout, {"next_page": '/'}),
 ]
