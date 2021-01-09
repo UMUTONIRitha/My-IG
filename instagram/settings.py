70e201a887ea7d8d816f40871b9df3fe5a78a985
UMUTONIRitha <rithamarie9@gmail.com>
2021-01-09 17:30:03 +0200

    final commit

--- instagram/settings.py
+++ instagram/settings.py
@@ -7,7 +7,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
-from pathlib import Path
+
 from decouple import config
 import os
 import django_heroku
